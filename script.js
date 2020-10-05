450046ef5fa09c92f73613ce5487cac9b01f2568
Dmannen03 <david.tornqvist@botkyrka.skola.se>
2020-10-05 14:04:42 +0200

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const input = document.getElementById("inputField");
 const output = document.getElementById("output");
 
+function translateRna(x){
+    if(x === "g"){return("c");}
+    else if(x === "c"){return("g");}
+    else if(x === "t"){return("a");}
+    else if(x === "a"){return("u");}
+    else {return("x");}
+}
+
+function convertToRna(str){
+    const lower = str.toLowerCase();
+    const letters = lower.split("");
+    const RNA = letters.map(translateRna);
+
+    return(RNA.join(""));
+   
+
+}
+
 function readAndDisplay(){
-    output.innerHTML = input.value.replace(/\n/g, "<br>");
+    const userinput = input.value;
+    const nucleoids =  convertToRna(userinput);
+    // const output = nucleoids.join("");
+
+    output.innerHTML = nucleoids;
 }
 
 input.addEventListener("input",readAndDisplay);
